c17cf35cf6e2602bfa3c56783efb1428861e97b4
Jacob Florczak <krarup214@gmail.com>
2016-10-10 10:43:34 +0200

    items

---
+++ spil/spil/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spil
+{
+    public class Items
+    {
+        public string navn;
+        public string beskrivelse; 
+
+
+        public Items(string beskrivelse, string navn);
+    }
+}
